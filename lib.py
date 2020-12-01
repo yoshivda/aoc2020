c9c67a5b4ee00e60b7d9d04c6d0ca1194ea31c26
Yoshi van den Akker <y.a.w.vandenakker@student.tudelft.nl>
2020-12-01 22:11:32 +0100

    Extract filename

--- lib.py
+++ lib.py
@@ -6,7 +6,7 @@ ROOT_PATH = Path(__file__).parents[0]
 
 
 def load_input(day):
-    path = Path(ROOT_PATH, f"inputs/day{str(day).zfill(2)}.txt")
+    path = Path(ROOT_PATH, get_filename(day))
     if not path.exists():
         download_input(day)
     return path.read_text()
@@ -16,7 +16,11 @@ def download_input(day):
     Path(ROOT_PATH, "inputs").mkdir(exist_ok=True)
     url = f"https://adventofcode.com/{YEAR}/day/{day}/input"
     req = requests.get(url, cookies={"session": Path(ROOT_PATH, f"credentials.secret").read_text().strip()})
-    Path(ROOT_PATH, f"inputs/day{str(day).zfill(2)}.txt").write_bytes(req.content)
+    Path(ROOT_PATH, get_filename(day)).write_bytes(req.content)
+
+
+def get_filename(day):
+    return f"inputs/day{str(day).zfill(2)}.txt"
 
 
 if __name__ == '__main__':
