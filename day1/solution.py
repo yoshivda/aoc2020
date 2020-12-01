5be42a2486765fb5ed1d8399559f786835bb13b5
Yoshi van den Akker <y.a.w.vandenakker@student.tudelft.nl>
2020-12-01 06:22:16 +0100

    Add day 1 part 2

--- day1/solution.py
+++ day1/solution.py
@@ -3,10 +3,27 @@
 def solve(filename):
     with open(filename) as f:
        nums = [int(line) for line in f]
+    # return part_one(nums)
+    return part_two(nums)
+
+
+def part_one(nums):
     for i in range(len(nums)):
         for j in range(len(nums)):
-            if i != j and nums[i] + nums[j] == 2020:
+            if i == j:
+                continue
+            if nums[i] + nums[j] == 2020:
                 return nums[i] * nums[j]
+
+
+def part_two(nums):
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            for k in range(len(nums)):
+                if i == j or i == k or j == k:
+                    continue
+                if nums[i] + nums[j] + nums[k] == 2020:
+                    return nums[i] * nums[j] * nums[k]
 
 
 if __name__ == "__main__":
