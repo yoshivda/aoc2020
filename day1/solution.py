83385e894fd773dd5091e8949237528049e659b2
Yoshi van den Akker <y.a.w.vandenakker@student.tudelft.nl>
2020-12-01 06:17:00 +0100

    Add day 1 part 1

--- day1/solution.py
+++ day1/solution.py
@@ -1,6 +1,12 @@
 
 
 def solve(filename):
+    with open(filename) as f:
+       nums = [int(line) for line in f]
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if i != j and nums[i] + nums[j] == 2020:
+                return nums[i] * nums[j]
 
 
 if __name__ == "__main__":
