18572f233112370dd92392fc0df2b77cf2b3777f
Yoshi van den Akker <y.a.w.vandenakker@student.tudelft.nl>
2020-12-17 17:57:57 +0100

    Generalise day 17 for n dimensions

--- days/day17.py
+++ days/day17.py
@@ -1,68 +1,49 @@
 import copy
-from collections import defaultdict
+from itertools import product
+from operator import itemgetter
 
 from lib import load_input
 
 
 def solve(data):
-    # return part_one(data.splitlines())
-    return part_two(data.splitlines())
+    # return solve_dimensions(data.splitlines(), 3)
+    return solve_dimensions(data.splitlines(), 4)
 
 
-def active_positions_around(x, y, z, levels):
-    return sum(1 for dx in (-1, 0, 1)
-               for dy in (-1, 0, 1)
-               for dz in (-1, 0, 1)
-               if not dx == dy == dz == 0
-               and levels[(x + dx, y + dy, z + dz)] == "#")
+def get_all_positions(mins, maxs):
+    res = [(x,) for x in range(mins[0] - 1, maxs[0] + 2)]
+    for i in range(1, len(mins)):
+        res = {(*prev, x) for prev in res for x in range(mins[i] - 1, maxs[i] + 2)}
+    return res
 
 
-def part_one(data):
-    levels = defaultdict(lambda: ".")
-    levels.update({(x, y, 0): data[y][x] for x in range(len(data[0])) for y in range(len(data))})
+def active_positions_around(pos, active, dimensions):
+    res = 0
+    for offset in product([-1, 0, 1], repeat=dimensions):
+        if offset != (0, ) * dimensions and tuple(map(sum, zip(pos, offset))) in active:
+            res += 1
+
+            # Short circuit, we don't care about larger values (saves 15% runtime)
+            if res > 3:
+                return 4
+    return res
+
+
+def solve_dimensions(data, dimensions):
+    active = {(x, y, *(0 for _ in range(dimensions - 2))) for x in range(len(data[0])) for y in range(len(data))
+              if data[y][x] == "#"}
     for i in range(6):
-        new_levels = copy.copy(levels)
-
-        for z in range(-i - 1, i + 2):
-            for y in range(-i - 1, len(data) + i + 1):
-                for x in range(-i - 1, len(data[0]) + i + 1):
-                    apa = active_positions_around(x, y, z, levels)
-                    if levels[(x, y, z)] == "#" and not (2 <= apa <= 3):
-                        new_levels[(x, y, z)] = "."
-                    elif levels[(x, y, z)] == "." and apa == 3:
-                        new_levels[(x, y, z)] = "#"
-        levels = new_levels
-
-    return sum(1 for val in levels.values() if val == "#")
-
-
-def active_positions_around_w(x, y, z, w, levels):
-    return sum(1 for dx in (-1, 0, 1)
-               for dy in (-1, 0, 1)
-               for dz in (-1, 0, 1)
-               for dw in (-1, 0, 1)
-               if not dx == dy == dz == dw == 0
-               and levels[(x + dx, y + dy, z + dz, w + dw)] == "#")
-
-
-def part_two(data):
-    levels = defaultdict(lambda: ".")
-    levels.update({(x, y, 0, 0): data[y][x] for x in range(len(data[0])) for y in range(len(data))})
-    for i in range(6):
-        new_levels = copy.copy(levels)
-
-        for w in range(-i - 1, i + 2):
-            for z in range(-i - 1, i + 2):
-                for y in range(-i - 1, len(data) + i + 1):
-                    for x in range(-i - 1, len(data[0]) + i + 1):
-                        apa = active_positions_around_w(x, y, z, w, levels)
-                        if levels[(x, y, z, w)] == "#" and not (2 <= apa <= 3):
-                            new_levels[(x, y, z, w)] = "."
-                        elif levels[(x, y, z, w)] == "." and apa == 3:
-                            new_levels[(x, y, z, w)] = "#"
-        levels = new_levels
-
-    return sum(1 for val in levels.values() if val == "#")
+        new_active = copy.copy(active)
+        mins = [min(active, key=itemgetter(x))[x] for x in range(dimensions)]
+        maxs = [max(active, key=itemgetter(x))[x] for x in range(dimensions)]
+        for pos in get_all_positions(mins, maxs):
+            apa = active_positions_around(pos, active, dimensions)
+            if pos in active and not (2 <= apa <= 3):
+                new_active.remove(pos)
+            elif pos not in active and apa == 3:
+                new_active.add(pos)
+        active = new_active
+    return len(active)
 
 
 if __name__ == "__main__":
